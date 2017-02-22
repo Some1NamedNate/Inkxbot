d7fba9cb0aa9acbd9ee50daa07feda84d6f099b1
InkxtheSquid <InkxtheSquid@users.noreply.github.com>
2017-02-21 19:35:23 -0800

    Update Inkxbot.py
    changes to the code

--- Inkxbot.py
+++ Inkxbot.py
@@ -27,26 +27,6 @@ startup_extensions = ["Inkxbotcogs.SplatoonCog",
                       ]
 
 
-discord_logger = logging.getLogger('discord')
-discord_logger.setLevel(logging.CRITICAL)
-log = logging.getLogger()
-log.setLevel(logging.INFO)
-handler = logging.FileHandler(filename='inkxbot.log', encoding='utf-8', mode='w')
-log.addHandler(handler)
-
-help_attrs = dict(hidden=True)
-
-
-# this line below is for future purposes, it's not gonna be used yet
-prefix = [',', '.']
-
-bot = commands.Bot(command_prefix=',', description=description, pm_help=True, help_attrs=help_attrs)
-
-
-def load_messages():
-    with open('servermessage.json') as m:
-        return json.load(m)
-
 @bot.event
 async def on_member_join(member):
     server = member.server
@@ -70,7 +50,7 @@ async def on_ready():
 @bot.event
 async def on_member_ban(member):
     for channel in member.server.channels:
-        if channel.name == "ban-logs":
+        if channel.name == "banlogs":
             await bot.send_message(channel, content="**BAN** \n**User**: {0}".format(member))
             break
 @bot.event
@@ -140,8 +120,147 @@ def load_credentials():
 
 async def post_on_discordlist():
     credentials = load_credentials()
+    listtoken = credentials['listtoken']    
+    payload = {"token": listtoken,
+               "servers": len(bot.servers)
+              }
+    
+    listurl = "https://bots.discordlist.net/api.php"
+    resp = await aiohttp.post(listurl, data=payload)
+    resp.close()
+
+if __name__ == '__main__':
+    credentials = load_credentials()
     token = credentials['token']    
-    payload = {"token": token,
+    bot.client_id = credentials['client_id']
+    bot.commands_used = Counter()
+    for extension in startup_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            print('Failed to load extension {}\n{}: {}'.format(extension, type(e).__name__, e))
+
+    bot.run(token)
+    handlers = log.handlers[:]
+    for hdlr in handlers:
+        hdlr.close()
+        log.removeHandler(hdlr)
+
+discord_logger = logging.getLogger('discord')
+discord_logger.setLevel(logging.CRITICAL)
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+handler = logging.FileHandler(filename='inkxbot.log', encoding='utf-8', mode='w')
+log.addHandler(handler)
+
+help_attrs = dict(hidden=True)
+
+
+# this line below is for future purposes, it's not gonna be used yet
+#prefix = [',', '.']
+
+bot = commands.Bot(command_prefix=',', description=description, pm_help=True, help_attrs=help_attrs)
+
+
+def load_messages():
+    with open('servermessage.json') as m:
+        return json.load(m)
+
+@bot.event
+async def on_member_join(member):
+    server = member.server
+    messages = load_messages()
+    if server.id not in messages: return
+    if "welcome" not in messages[server.id]: return
+    srv = messages[server.id]
+    wlc = messages[server.id]['welcome'].format(member)
+    await bot.send_message(server, wlc)
+
+@bot.event
+async def on_ready():
+    print('Inkxbot is logged in and online!')
+    print('--------------------------------')
+    await bot.change_presence(game=discord.Game(name='https://inkxbot.wordpress.com'))
+    #await bot.send_message(discord.Object(id='248106639410855936'), "@here, A new WordPress post about my development has been made, check it out at <https://inkxbot.wordpress.com/>")
+    #await bot.send_message(discord.Object(id='227514633735372801'), "A new WordPress post about my development has been made, check it out at <https://inkxbot.wordpress.com/>")
+    #await bot.send_message(discord.Object(id='258350226279104512'), "A new WordPress post about my development has been made, check it out at <https://inkxbot.wordpress.com/>")
+
+
+@bot.event
+async def on_member_ban(member):
+    for channel in member.server.channels:
+        if channel.name == "banlogs":
+            await bot.send_message(channel, content="**BAN** \n**User**: {0}".format(member))
+            break
+@bot.event
+async def on_message(message):
+    #~ lm = load_messages()
+    #~ server = message.server
+    #~ svr = lm[server.id]
+    #~ author = message.author.mention
+    #~ shill = lm[server.id]["severShill"]
+    #~ forbode = lm[server.id]["forbode"]
+    #~ shillmsg = shill.format(author)
+    #~ fbmsg = forbode.format(author)
+	#~ notdeadem = discord.Embed(title="", color=0xFF8C00))
+
+    if message.content.startswith('+rip Inkxbot'):
+        await asyncio.sleep(8)
+        await bot.send_typing(message.channel)
+        await asyncio.sleep(2)
+        await bot.send_message(message.channel, "That's a lie.")
+
+    elif message.content.startswith('+rip <@245648163837444097>'):
+        await asyncio.sleep(8)
+        await bot.send_typing(message.channel)
+        await asyncio.sleep(2)
+        await bot.send_message(message.channel, "That's a lie.")
+    
+    elif message.content.startswith('+kill <@245648163837444097>'):
+        await asyncio.sleep(8)
+        await bot.send_typing(message.channel)
+        await asyncio.sleep(1)
+        await bot.send_message(message.channel, "Sticks and Stones may break my bolts, but lazer cannons never hurt me.")
+
+    elif message.content.startswith('+kill Inkxbot'):
+        await asyncio.sleep(8)
+        await bot.send_typing(message.channel)
+        await asyncio.sleep(1)
+        await bot.send_message(message.channel, "Sticks and Stones may break my bolts, but lazer cannons never hurt me.")
+
+    elif message.content.startswith('<@245648163837444097>'):
+        await bot.send_typing(message.channel)
+        await asyncio.sleep(1)
+        await bot.send_message(message.channel, "What the hell do you want from me. ~~type ,inkxbot you nerd~~")
+
+    elif message.content.startswith('(╯°□°）╯︵ ┻━┻'):    
+        await bot.send_message(message.channel, "┬─┬﻿ ノ( T_Tノ)")
+
+    elif message.content.startswith('/tableflip'):
+        await bot.send_message(message.channel, "┬─┬﻿ ノ( T_Tノ)")
+
+    elif message.content.startswith(',,'):
+        await bot.send_message(message.channel, "My prefix changed you nerd. It's just one comma now")
+        
+
+    #~ elif invite_syntax.search(message.content):
+        #~ if server.id == svr:
+            #~ if server.id not in svr: return
+            #~ if "forbode" not in svr: return
+            #~ await bot.send_message(message.channel, fbmsg)
+            #~ await asyncio.sleep(1)
+            #~ await bot.send_message(message.channel, shillmsg)
+
+    await bot.process_commands(message)
+
+def load_credentials():
+    with open('credentials.json') as f:
+        return json.load(f)
+
+async def post_on_discordlist():
+    credentials = load_credentials()
+    listtoken = credentials['listtoken']    
+    payload = {"token": listtoken,
                "servers": len(bot.servers)
               }
     
