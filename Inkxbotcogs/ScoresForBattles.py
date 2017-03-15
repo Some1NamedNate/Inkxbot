30826dfc740ab2c825b3330d6cf1191d92ea96d8
InkxtheSquid <InkxtheSquid@users.noreply.github.com>
2017-03-15 15:50:12 -0700

    Add files via upload

--- Inkxbotcogs/ScoresForBattles.py
+++ Inkxbotcogs/ScoresForBattles.py
@@ -1,12 +1,8 @@
 from discord.ext import commands
+from .utils import checks
 import discord
 import asyncio
 import json
-
-
-def write_teams():
-    with open('teams.json') as f:
-        return json.dump()
 
 def load_teams():
     with open('teams.json') as l:
@@ -23,8 +19,9 @@ class Scoring:
         self.bot = bot
 
     @commands.command(aliases=['p'], pass_context=True, no_pm=True, hidden=False)
+    @checks.mod_or_permissions()
     async def post(self, ctx, battle, homescr, args, awayscr):
-        """posts a scrim score in a #scrim-scores channel \n EXAMPLE: ,post 'Tournament or Scrim' 2 'name of clan you scrimed against' 1"""
+        """posts a scrim score in a #scrim-scores channel \n You will need a 'Bot Commander' role in order to use this \n EXAMPLE: ,post 'Tournament or Scrim' 2 'name of clan you scrimed against' 1"""
         teams = load_teams()
         trnys = load_trnynames()
         if battle == 'scrim':
@@ -51,30 +48,22 @@ class Scoring:
                 await self.bot.say("You haven't given me your team's name, type `,writeteam \"YOUR TEAM'S NAME HERE\"` to store it into my database")
                 break
 
-            elif battle not in trnys:
-                if s.name == "tournament-scores":
-                    await self.bot.send_message(s, "**Tournament**: {4} \n{0} {1}  -  {3} {2}".format(teamname, homescr, args, awayscr, battle))
-                    await self.bot.say("The tourament's name is not in my database yet but I'll still post it.")
-                    break
-
         else:
             await self.bot.say("It seems there's a problem, try again")
 
     @commands.command(aliases=['wt'], pass_context=True, no_pm=True, hidden=False)
+    @checks.mod_or_permissions()
     async def writeteam(self, ctx, args):
-        """I will write the name of your clan to my database"""
+        """I will write the name of your clan to my database \n You will need a 'Bot Commander' role in order to use this"""
         server = ctx.message.server
-        user = ctx.message.author
-#                In the future, the command will write to the json by itself. But for now, I have to do it.
-        #data = {'{}'.format(server.id):{'team': '{}'.format(args)}
-        #        }
-        #file = open('teams.json', 'w')
-        #json.dump(obj=data, fp=file, indent=4)
-        #await asyncio.sleep(1)
-        #await self.bot.say("I have successfully written your team's name into my database")
-        print('"{0}: {1}" Requested by {2}'.format(server.id, args, user))
-        print('----------------------------------------------')
-        await self.bot.say("the content will be added to my database in a while")
+        teams = load_teams()
+        d = teams
+        d[server.id] = {}
+        d[server.id]["team"] = args
+        with open('teams.json', 'w') as fp:
+            json.dump(d, fp, indent=2)
+        await asyncio.sleep(1)
+        await self.bot.say("I have successfully written your team's name into my database")
 
 def setup(bot):
     bot.add_cog(Scoring(bot))
--- Inkxbotcogs/modcog.py
+++ Inkxbotcogs/modcog.py
@@ -3,12 +3,16 @@ from .utils import config, checks
 import asyncio
 import discord
 
+def load_messages():
+    with open('servermessage.json') as m:
+        return json.load(m)
+
 class Moderation:
     """ Moderative Commands to keep the server clean """
 
     def __init__(self, bot):
         self.bot = bot
-    
+
 
 
     def _role_from_string(self, server, rolename, roles=None):
@@ -18,11 +22,11 @@ class Moderation:
 
         return role
 
-    
+
     @commands.command(pass_context=True, no_pm=True, name = 'clear')
     @commands.has_role('Bot Commander')
     async def _clear(self, ctx, amount : int = 100):
-        """Deletes my messages."""
+        """Deletes my messages. \n You will need a 'Bot Commander' role in order to use this"""
 		
         channel = ctx.message.channel
 
@@ -37,23 +41,25 @@ class Moderation:
         await self.bot.say('Deleted {0}'.format(calls))
         await asyncio.sleep(1.5)
         await self.bot.delete_message(channel, limit=1, after=ctx.message)
-        
+
     @commands.command(pass_context=True, no_pm=True)
-    @commands.has_role('Bot Commander')
+    @checks.mod_or_permissions()
     async def purge(self, ctx, amount : int = 5):
-        """Deletes specified number of messages."""
+        """Deletes specified number of messages.
+        You must have a 'Bot Commander' role in order to use this"""
         await self.bot.purge_from(ctx.message.channel, limit=amount, before=ctx.message)
         await self.bot.say('Deleted {0}'.format(amount))
         await asyncio.sleep(1.5)
         #await self.bot.delete_message(ctx.message.channel, limit=(1), after=ctx.message)
 
 #    the command below is still in development. I'm sorry if the development is gonna take long
-    
+
     @commands.command(pass_context=True, no_pm=True)
     @commands.has_role('Bot Commander')
     async def give(self, ctx, rolename, user: discord.Member=None):
         """Gives a role to a user, defaults to author
-        Role name must be in quotes if there are spaces."""
+        Role name must be in quotes if there are spaces.
+        You must have a 'Bot Commander' role in order to use this"""
         author = ctx.message.author
         channel = ctx.message.channel
         server = ctx.message.server
@@ -75,10 +81,11 @@ class Moderation:
         await self.bot.say('I have given the {} role to {}'.format(role.name, user.name))
 
     @commands.command(pass_context=True, no_pm=True, name = 'remove')
-    @commands.has_role('Bot Commander')
+    @checks.mod_or_permissions()
     async def _remove(self, ctx, rolename, user: discord.Member=None):
         """Removes a role from user, defaults to author
-        Role name must be in quotes if there are spaces."""
+        Role name must be in quotes if there are spaces.
+        You will need a 'Bot Commander' role in order to use this"""
         server = ctx.message.server
         author = ctx.message.author
 
@@ -98,6 +105,22 @@ class Moderation:
                 await self.bot.say("I don't have permissions to manage roles!")
         else:
             await self.bot.say("User does not have that role.")
-    
+
+    #@commands.group(pass_context=True, no_pm=True)
+    #@checks.mod_or_permissions()
+    #async def welcome(self, ctx)
+        #"""Shows your server's current welcome message or changes it. Bot Commander required"""
+        #server = ctx.message.server
+        #lm = load_messages()
+        #wlc = lm[server.id]['welcome'].format('user')
+        #await self.bot.say("**your server's current welcome message:** `{}`".format(wlc))
+
+    #@welcome.command(pass_context=True, no_pm=True)
+    #@checks.mod_or_permissions()
+    #async def onjoin(self, ctx, args)
+        #"""Sets the server's welcome message to when a new user joins the server"""
+        #server= ctx.message.server
+        #lm = load_messages()
+
 def setup(bot):
     bot.add_cog(Moderation(bot))
