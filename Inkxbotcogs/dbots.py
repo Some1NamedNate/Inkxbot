f3c2d4a4300b5e77f32c0279096f1606520dd8b1
Inkx <inkxthesquid@mail.com>
2017-05-23 14:02:18 -0700

    Update dbots.py

--- Inkxbotcogs/dbots.py
+++ Inkxbotcogs/dbots.py
@@ -1,36 +1,69 @@
 import aiohttp
 import json
 import logging
+import discord
+
 
 log = logging.getLogger()
 
-DISCORDLIST_API = 'https://bots.discordlist.net/api'
+DISCORDLISTAPI   = 'https://bots.discordlist.net/api.php'
+CARBONAPIBOTDATA = 'https://www.carbonitex.net/discord/data/botdata.php'
+DBOTSAPI         = 'https://bots.discord.pw/api'
 
 class Discordlist:
-    """Cog for updating discordlist.net bot information."""
+    """Cog for updating bot information on botlisting websites."""
     def __init__(self, bot):
         self.bot = bot
         self.session = aiohttp.ClientSession()
 
     def __unload(self):
-        # pray it closes
+        # give me hope
         self.bot.loop.create_task(self.session.close())
 
     async def update(self):
-        payload = {
-            "token": '(discordlist token goes here)',
-            "servers": len(self.bot.servers)
-        }
-        url = "https://bots.discordlist.net/api.php"
-        resp = await aiohttp.post(url, data=payload)
-        resp.close()
-        async with self.session.post(url, data=payload) as resp:
-            log.info('DiscordList statistics returned {0.status} for {1}'.format(resp, payload))
+        # Matt hasn't given me a key yet
+    #    carbon_payload = {
+            #'key': self.bot.carbon_key,
+            #'servercount': len(self.bot.servers)
+        #}
 
-    async def on_server_join(self, server):
+        #async with self.ses sion.post(CARBONAPIBOTDATA, data=carbon_payload) as resp:
+            #log.info('Carbon statistics returned {0.status} for {1}'.format(resp, carbon_payload))
+
+        payload = json.dumps({
+            'server_count': len(self.bot.guilds)
+        })
+
+        headers = {
+            'authorization': self.bot.dbots_key,
+            'content-type': 'application/json'
+        }
+
+        url = '{0}/bots/{1.user.id}/stats'.format(DBOTSAPI, self.bot)
+        async with self.session.post(url, data=payload, headers=headers) as resp:
+            log.info('DBots statistics returned {0.status} for {1}'.format(resp, payload))
+
+
+        dlpayload = {
+            "token": self.bot.discordlist_token,
+            "servers": len(self.bot.guilds)
+        }
+
+        serverdata = {
+            "servers": len(self.bot.guilds)
+        }
+
+        url = "https://bots.discordlist.net/api.php"
+        resp = await aiohttp.post(url, data=dlpayload)
+        resp.close()
+        async with self.session.post(url, data=dlpayload) as resp:
+            log.info('DiscordList statistics returned {0.status} for {1}'.format(resp, serverdata))
+
+    async def on_guild_join(self, guild):
+        await guild.send("Thank you for adding me to your server! I'll be a plessure to help with many things for you! type `,help` for information on my commands!")
         await self.update()
 
-    async def on_server_remove(self, server):
+    async def on_guild_remove(self, guild):
         await self.update()
 
     async def on_ready(self):
